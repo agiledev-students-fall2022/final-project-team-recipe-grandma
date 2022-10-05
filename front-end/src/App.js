325dacf1d4409d6328d8c8d6b14ddd7f15478e21
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-10-05 02:05:14 -0400

    Added flow changes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): React.Node {
   return (
     <div className="App">
       <header className="App-header">
