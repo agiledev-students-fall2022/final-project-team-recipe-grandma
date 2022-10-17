afd48a6cc0a631b93156405c8dd1f406ed6f01f2
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-10-17 17:59:44 -0400

    Added eslint hot reload && frontend project dependencies

---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+function App(): React.Node {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
