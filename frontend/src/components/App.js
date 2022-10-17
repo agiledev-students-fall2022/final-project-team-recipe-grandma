f0e8aecaf9b49084a1c291f6ca8e34e1470166aa
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-10-17 19:43:34 -0400

    Ported frontend code to new build

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Recipe from './Recipe';
 import './App.css';
 
 function App(): React.Node {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios( // found a recipe database with ingredients + cooking steps
+      // but it gives me an error message
+      // "If this is valid SVG, it's probably a bug in svg-parser."
+        'https://raw.githubusercontent.com/raywenderlich/recipes/master/Recipes.json',
+      );
+      setData(result.data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <h1>Recommended Recipes</h1>
+      <section className="recipes">
+        {data.map((item) => (
+          <Recipe key={item.name} details={item} />
+        ))}
+      </section>
+    </>
   );
 }
 
