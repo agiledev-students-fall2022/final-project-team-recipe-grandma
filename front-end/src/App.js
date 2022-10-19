4a6e09c9e20d18c28384c116b560d9553ad5b899
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-10-18 23:58:34 -0400

    Added basic Navbar and removed frontend test folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,14 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Recipe from './Recipe';
+// import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App(): React.Node {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios( // found a recipe database with ingredients + cooking steps
-      // but it gives me an error message
-      // "If this is valid SVG, it's probably a bug in svg-parser."
-        'https://raw.githubusercontent.com/raywenderlich/recipes/master/Recipes.json',
-      );
-      setData(result.data);
-    }
-    fetchData();
-  }, []);
-
   return (
     <>
-      <h1>Recommended Recipes</h1>
-      <section className="recipes">
-        {data.map((item) => (
-          <Recipe key={item.name} details={item} />
-        ))}
-      </section>
+      <Navbar />
+      <footer />
     </>
   );
 }
