576188e0ad584acc13f2daaae4442e35cfd73750
NghiaNim <ntn6039@nyu.edu>
2022-10-21 21:48:34 -0400

    Added a component file

--- front-end/src/pages/SearchIngredients/index.js
+++ front-end/src/pages/SearchIngredients/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+// import SearchBar from '../../components/SearchBar';
 
 // type Props = { };
 
 function SearchIngredients(): React.Node {
   return (
     <h1>SearchPage</h1>
+    // SearchBar.SearchBar()
   );
 }
 
