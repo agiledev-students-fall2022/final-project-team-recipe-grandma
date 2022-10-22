4281b762231e17c69e1b77b826fc6ff96f615db0
NghiaNim <ntn6039@nyu.edu>
2022-10-21 22:09:01 -0400

    added a textbox

--- front-end/src/components/SearchBar/index.js
+++ front-end/src/components/SearchBar/index.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 
 function SearchBar(): React.Node {
   return (
-    <h1>SearchPage</h1>
+    <div className="SearchBar">
+      <h1>Add your ingredients</h1>
+      <input type="text" />
+    </div>
   );
 }
 
