e7fc9f6c675bd4f3acb247201cdf1b7e8cd1a55e
yeonieheoo <sh5874@nyu.edu>
2022-10-23 22:38:36 -0400

    (Routed) a selected RecipeInDetail in a page

--- front-end/src/pages/RecipeInDetail/index.js
+++ front-end/src/pages/RecipeInDetail/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import * as React from 'react';
-
 import { useState, useEffect } from 'react';
-
-import Recipe from '../../components/RecipeInDetail';
+import { useParams } from 'react-router-dom';
+// import Recipe from '../../components/RecipeInDetail';
 
 function RecipeInDetail(): React.Node {
+  // const navigate = useNavigate();
+  const { index } = useParams();
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -13,17 +14,21 @@ function RecipeInDetail(): React.Node {
       const result = await axios(
         'https://raw.githubusercontent.com/raywenderlich/recipes/master/Recipes.json',
       );
+      console.log(result.json());
       setData(result.data);
     }
     fetchData();
-  }, []);
+  }, [data]);
+
+  // finding the index of the selected recipe
+  console.log(data, index);
   return (
     <>
-      <h1>Your Recipe</h1>
+      {/* <h1></h1> */}
       <section className="recipes">
-        {data.map((item) => (
-          <Recipe key={item.name} details={item} />
-        ))}
+        <p>{data[index].ingredients}</p>
+        <p>{data[index].steps}</p>
+        <p>{data[index].imageURL}</p>
       </section>
     </>
   );
