0aa544e885e6e0b68c4927594210fd5c382a8da3
geontackee <95744496+geontackee@users.noreply.github.com>
2022-10-24 01:26:35 -0400

    Created ReviewForm and SingleReview component

---
+++ front-end/src/components/ReviewComment/SingleReview.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import './SingleReview.css';
+
+type Props = $ReadOnly<{|
+  review: {
+    id: string,
+    body: string,
+    stars: number,
+    username: string,
+    userId: string,
+    parentId: string,
+    createdAt: string
+  }
+|}>;
+
+function SingleReview(props: Props): React.Node {
+  const { review } = props;
+  return (
+    <article className="review">
+      <h3 className="username">{review.username}</h3>
+      {[...Array(review.stars)].map(() => (<h6 className="stars">⭐</h6>))}
+      <h4 className="body">{review.body}</h4>
+      <h5 className="time">{review.createdAt}</h5>
+    </article>
+  );
+}
+
+export default SingleReview;
