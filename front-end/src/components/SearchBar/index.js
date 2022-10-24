f8568ff30a7f0f6a69090d1dbb45b4020ad02250
JenniferZheng0430 <jennifer.zeng20@cflsap.com>
2022-10-23 21:15:06 -0400

    search bar add item function

--- front-end/src/components/SearchBar/index.js
+++ front-end/src/components/SearchBar/index.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
+import { useState } from 'react';
 
 function SearchBar(): React.Node {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleClick = () => {
+    // value of input field
+    console.log('handleClick: ', search);
+  };
+
   return (
     <div className="SearchBar">
       <h1>Add your ingredients</h1>
-      <input type="text" />
+      <input type="text" name="search" onChange={handleChange} value={search} />
+      <button type="button" className="search-button" onClick={handleClick}>add</button>
     </div>
   );
 }
