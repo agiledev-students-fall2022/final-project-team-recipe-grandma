734e362352c115718d5b41fa7026d7358681f993
NghiaNim <ntn6039@nyu.edu>
2022-10-24 00:57:52 -0400

    added search function and remove function

--- front-end/src/pages/SearchIngredients/index.js
+++ front-end/src/pages/SearchIngredients/index.js
@@ -1,14 +1,23 @@
 import * as React from 'react';
+import { useState } from 'react';
 import SearchBar from '../../components/SearchBar';
 import IngredientsList from '../../components/IngredientsList/IngredientsList';
 
-// type Props = { };
-
 function SearchIngredients(): React.Node {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredients = (item) => {
+    setIngredients((current) => [...current, item]);
+  };
+
+  const removeItem = (index) => {
+    setIngredients((oldState) => oldState.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
-      <SearchBar />
-      <IngredientsList />
+      <SearchBar onAction={addIngredients} />
+      <IngredientsList onAction={removeItem} ingredients={ingredients} />
     </div>
   );
 }
