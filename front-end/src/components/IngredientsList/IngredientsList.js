080a08939d960ee0c00a3fb9aef6fc023face55c
JenniferZheng0430 <jennifer.zeng20@cflsap.com>
2022-10-23 20:36:07 -0400

    ingredients list bugs fix

--- front-end/src/components/IngredientsList/IngredientsList.js
+++ front-end/src/components/IngredientsList/IngredientsList.js
@@ -3,33 +3,32 @@ import './IngredientsList.css';
 import * as React from 'react';
 
 function IngredientsList(): React.Node {
-  const initialState = ['apple','peach','orange'];
+  const initialState = ['apple', 'peach', 'orange'];
   // var noIngredients = false;
   const [state, setState] = useState(initialState);
 
-  function removeItem(index) {
+  const removeItem = (index) => {
     // var newState = state.splice(index, 1);
     setState((oldState) => oldState.filter((_, i) => i !== index));
 
     console.log(state);
-  }
+  };
 
   return (
     <div>
       <h4>Added Ingredients List</h4>
-    
       <ul className="list-group">
-        {state.map((item, i) => 
+        {state.map((item, i) => (
           <li className="list-group-item">
             <div className="list-box">
               <p className="left">{item}</p>
               <button type="button" className="right" onClick={() => removeItem(i)}>
                 remove
               </button>
-              <div className="clear"></div>
+              <div className="clear" />
             </div>
           </li>
-        )}
+        ))}
       </ul>
     </div>
   );
