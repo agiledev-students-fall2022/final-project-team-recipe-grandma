c926faeedf4d7d07c87b95be764bc979427c1f90
OyungerelA <oa853@nyu.edu>
2022-10-25 14:39:58 -0400

    remove listed links in navbar and create search button on home page

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,13 +1,17 @@
-// import axios from 'axios';
-
-import * as React from 'react';
 // import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+import * as React from 'react';
+import {
+  useNavigate, Route, Routes,
+} from 'react-router-dom';
 // fetched all data from the API below into a file: '../../temp_recipedata.json'
 import data from '../../temp_recipedata.json';
-
 import Recipe from '../../components/Recipe';
+import CustomButton from '../../components/Button';
+import SearchIngredients from '../SearchIngredients';
+
 import '../../components/Recipe/aRecipeButtonStyle.css';
+import './Home.css';
 
 function Home(): React.Node {
   // const [data, setData] = useState([]);
@@ -24,8 +28,13 @@ function Home(): React.Node {
 
   const navigate = useNavigate();
 
+  const goToSearchPage = () => {
+    navigate('/search-ingredient');
+  };
+
   return (
     <>
+      <CustomButton className="search-btn" text="Search for ingredients" onAction={goToSearchPage} />
       {/* <h1>Recommended Recipes</h1> */}
       <section className="recipes">
         {data.map((item) => (
@@ -36,6 +45,10 @@ function Home(): React.Node {
           </article>
         ))}
       </section>
+
+      <Routes>
+        <Route path="/search-ingredient" element={<SearchIngredients />} />
+      </Routes>
     </>
   );
 }
