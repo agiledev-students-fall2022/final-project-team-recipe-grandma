b70a4a655765202e5554f1f6780d8731e8c73569
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-10-26 05:01:26 -0400

    Consolidated review mock data

--- front-end/src/pages/ReviewPage/ReviewPage.js
+++ front-end/src/pages/ReviewPage/ReviewPage.js
@@ -1,18 +1,24 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ReviewForm from '../../components/ReviewComment/ReviewForm';
-import data from '../../temp_recipedata.json';
 import './ReviewPage.css';
-import placeholderFood from './placeholder_food.jpg';
+import { fetchReviewData } from '../../util';
 /* Need to implement json file to correspond to each recipe */
 
 function ReviewPage(): React.Node {
   const { recipeindex } = useParams();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchReviewData(setData);
+    console.log(data);
+  }, []);
 
   return (
     <div className="ReviewPage">
-      <h1 className="title">{data[recipeindex].name}</h1>
-      <img className="foodImg" src={data[recipeindex].imageURL} alt={placeholderFood} />
+      <h1 className="title">{data.length >= 0 ?? data[recipeindex].name}</h1>
+      <img className="foodImg" src={data.length >= 0 ? data[recipeindex].imageURL : ''} alt="Food" />
       <ReviewForm />
     </div>
   );
