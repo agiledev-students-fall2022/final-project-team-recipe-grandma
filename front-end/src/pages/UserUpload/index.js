83690d8311604f7e781feb4039c316829b0d06a5
NghiaNim <ntn6039@nyu.edu>
2022-10-26 17:38:09 -0400

    made page run

--- front-end/src/pages/UserUpload/index.js
+++ front-end/src/pages/UserUpload/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function UserUpload(): React.Node {
-	return (
-		<h1>Hello World</h1>
-	);
+  return (
+    <h1>hellowhhhhhhhhhhhhhhh</h1>
+  );
 }
 
 export default UserUpload;
