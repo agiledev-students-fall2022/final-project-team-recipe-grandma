824269d990e3dffc71d11803738775b5a8fca56a
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-10-31 14:06:48 -0400

    Added .env and basic test route

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, Worlds!');
+const dotenv = require('dotenv').config({ path: require('find-config')('.env') });
+console.log(dotenv);
+const express = require('express');
+
+// CONSTANTS
+const port = process.env.API_PORT;
+
+const app = express();
+
+app.get('/rgapi/test', (req, res) => {
+  res.status(200).json({ message: 'Test works! '});
+});
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
