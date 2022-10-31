0035c0977bc418a914c8409cc298d5e6852805aa
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-10-31 14:43:37 -0400

    Added basic controller implementation

--- back-end/routes/UserRoutes.js
+++ back-end/routes/UserRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const UserController = require('../controllers/UserController');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  res.status(200).json({ message: 'Test works! '});
-});
+router.get('/test', UserController.TestControllerFunction);
 
 module.exports = router;
