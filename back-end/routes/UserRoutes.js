a41201cf4aa8a77f464598f09f3ec7a9b2d193cf
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-02 00:56:09 -0400

    Added user model and registration

--- back-end/routes/UserRoutes.js
+++ back-end/routes/UserRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
+
 const router = express.Router();
 
 router.get('/test', UserController.TestControllerFunction);
+router.post('/register', UserController.RegisterUser);
 
 module.exports = router;
