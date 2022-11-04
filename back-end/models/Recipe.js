170dbc51f62893ce8fa71350f89380bb023b4885
JenniferZheng0430 <jennifer.zeng20@cflsap.com>
2022-11-04 16:17:28 -0400

    added recipe.js in /model

---
+++ back-end/models/Recipe.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { addListener } = require('nodemon');
+
+const PostSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    id: {
+        type: Number,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: false  
+    },
+    ingredients: {
+        type: Array,
+        required: true
+    },
+    directions: {
+        type: Array,
+        required: false
+    }
+});
+
+module.exports = mongoose.model("Posts",PostSchema);
+
