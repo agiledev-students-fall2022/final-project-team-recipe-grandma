eea6aaea1a2455d82880382365adc2591fe945d6
yeonieheoo <sh5874@nyu.edu>
2022-11-04 22:50:52 -0400

    Fixed RecipeRoutes, Controller errors

--- back-end/routes/RecipeRoutes.js
+++ back-end/routes/RecipeRoutes.js
@@ -4,6 +4,6 @@ const RecipeController = require('../controllers/RecipeController');
 const router = express.Router();
 
 router.get('/test', RecipeController.TestRecipeFunction);
-router.get('/recipe', RecipeController.GetRecipe);
+router.post('/recipe', RecipeController.GetRecipe);
 
 module.exports = router;
