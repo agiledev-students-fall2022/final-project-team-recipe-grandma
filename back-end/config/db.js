d72ca78b4efae2a26198273e5c3aac665b74ef8a
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-11-04 21:42:13 -0400

    fixed db.js file missing URL

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(proccess.env.APP_DB_URL);
+    const conn = await mongoose.connect(process.env.APP_DB_URL);
     console.log(`MongoDB Connect: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
