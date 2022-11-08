dea1b146b8fe049a84bf82fcf03f6424aee1751c
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-07 23:05:43 -0500

    Updated test suite for backend

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.APP_DB_URL);
-    console.log(`MongoDB Connect: ${conn.connection.host}`);
+    console.log('Connecting to database...');
+    const admin = process.env.API_USN;
+    const adminPassword = process.env.API_PASS;
+    const conn = await mongoose.connect(process.env.APP_DB_URL
+      .replace('<password>', adminPassword)
+      .replace('<admin>', admin)
+      .replace('<username>', admin));
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
