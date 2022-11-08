f05140186d44f10977b0c3e0980bbe3699fec3a2
NghiaNim <ntn6039@nyu.edu>
2022-11-08 00:04:41 -0500

    added test for like controller

--- back-end/controllers/LikeController.js
+++ back-end/controllers/LikeController.js
@@ -3,28 +3,11 @@
 const Recipe = require('../models/Recipe');
 
 class LikeController {
-  static async TestRecipeFunction(req, res) {
+  static async TestLikeFunction(req, res) {
     if (req.body.text) {
       return res.status(400).json({ message: 'Bad request test!' });
     }
-    return res.status(200).json({ message: 'Hello, World! Recipes here!' });
-  }
-
-  static async CreateRecipe(req, res) {
-    const recipe = new Recipe({
-      name: req.body.name,
-      id: req.body.id,
-      photo: req.body.photo,
-      ingredients: req.body.ingredients,
-      directions: req.body.directions,
-      like: 0,
-    });
-    try {
-      const NewRecipe = await recipe.save();
-      res.status(201).send(NewRecipe);
-    } catch (err) {
-      res.status(400).json({ message: err });
-    }
+    return res.status(200).json({ message: 'Hello, World! Like here!' });
   }
 }
 
