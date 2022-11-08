800b16014db05bc2f69815d727d9fa7af13efb83
geontackee <95744496+geontackee@users.noreply.github.com>
2022-11-08 15:47:33 -0500

    Fixed error

--- back-end/models/Ingredient.js
+++ back-end/models/Ingredient.js
@@ -3,15 +3,11 @@ const mongoose = require('mongoose');
 const Ingredient = mongoose.Schema({
   ingredient: {
     type: String,
-<<<<<<< HEAD
-    required: true,
-=======
     required: [true, 'Please add a ingredient'],
   },
   type: {
     type: String,
     require: [true, 'Please add a ingredient type'],
->>>>>>> master
   },
 });
 
