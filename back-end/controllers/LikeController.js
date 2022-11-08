fbe20ca781c06ccc42311fd82c86c08629a89f0a
NghiaNim <ntn6039@nyu.edu>
2022-11-08 17:44:43 -0500

    added new delete and create likes

--- back-end/controllers/LikeController.js
+++ back-end/controllers/LikeController.js
@@ -1,6 +1,4 @@
-// const express = require('express');
-// const RecipeRouter = express.Router();
-// const Recipe = require('../models/Recipe');
+const Like = require('../models/Like');
 
 class LikeController {
   static async TestLikeFunction(req, res) {
@@ -10,9 +8,23 @@ class LikeController {
     return res.status(200).json({ message: 'Hello, World! Like here!' });
   }
 
-  static async Like(req, res) {
+  static async CreateLike(req, res) {
+    const like = new Like({
+      userId: req.body.userId,
+      recipeId: req.body.recipeId,
+    });
     try {
-      console.log('liked');
+      const NewLike = await like.save();
+      res.status(201).send(NewLike);
+    } catch (err) {
+      res.status(400).json({ message: err });
+    }
+  }
+
+  static async DeleteLike(req, res) {
+    try {
+      const RemoveLike = await Like.remove(req.params.userId);
+      res.json(RemoveLike);
     } catch (err) {
       res.json({ message: err.message });
     }
