1c5ddfe9ce4ef89b1e9587d7fd1126e4115c200e
JenniferZheng0430 <jennifer.zeng20@cflsap.com>
2022-11-07 23:21:29 -0500

    type added

--- back-end/models/Ingredient.js
+++ back-end/models/Ingredient.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const Ingredient = mongoose.Schema({
     ingredient: {
         type: String,
-        required: true,
+        required: [true, 'Please add a ingredient'],
     },
+    type: {
+        type: String,
+        require: [true, 'Please add a ingredient type'],
+    }
 });
 
 module.exports = mongoose.model('Ingredient', Ingredient);
