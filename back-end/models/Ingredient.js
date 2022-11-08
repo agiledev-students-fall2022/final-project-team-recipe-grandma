aabda77da83b68685ebcabc8c64c8c69ee2b8a31
JenniferZheng0430 <jennifer.zeng20@cflsap.com>
2022-11-07 23:28:38 -0500

    syntax bugs fixed

--- back-end/models/Ingredient.js
+++ back-end/models/Ingredient.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const Ingredient = mongoose.Schema({
-    ingredient: {
-        type: String,
-        required: [true, 'Please add a ingredient'],
-    },
-    type: {
-        type: String,
-        require: [true, 'Please add a ingredient type'],
-    }
+  ingredient: {
+    type: String,
+    required: [true, 'Please add a ingredient'],
+  },
+  type: {
+    type: String,
+    require: [true, 'Please add a ingredient type'],
+  },
 });
 
 module.exports = mongoose.model('Ingredient', Ingredient);
