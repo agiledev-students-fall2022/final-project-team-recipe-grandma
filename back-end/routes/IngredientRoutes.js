09fde6b4e7a14bb3dd4a6daf95748162accdd7c7
NghiaNim <ntn6039@nyu.edu>
2022-11-08 12:07:21 -0500

    fixed indentation

--- back-end/routes/IngredientRoutes.js
+++ back-end/routes/IngredientRoutes.js
@@ -4,6 +4,6 @@ const LikeController = require('../controllers/LikeController');
 const router = express.Router();
 
 router.get('/test', LikeController.TestLikeFunction);
-//router.post('/like', LikeController.Liked);
+// router.post('/like', LikeController.Liked);
 
 module.exports = router;
