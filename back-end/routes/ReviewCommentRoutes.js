9cf6893cac0eb27c71e4e7f355e8301bf278ea1e
geontackee <95744496+geontackee@users.noreply.github.com>
2022-11-08 22:32:48 -0500

    Added indexes to ReviewCommentRoutes

--- back-end/routes/ReviewCommentRoutes.js
+++ back-end/routes/ReviewCommentRoutes.js
@@ -3,7 +3,8 @@ const ReviewCommentController = require('../controllers/ReviewCommentController'
 
 const router = express.Router();
 
-router.get('/review', ReviewCommentController.GetReviewComment);
+router.get('/review/:index', ReviewCommentController.GetReviewComment);
+router.get('/review/:index1/:index2', ReviewCommentController.GetSingleReviewComment);
 router.get('/reviewTest', ReviewCommentController.TestReviewComment);
 router.post('/review/create', ReviewCommentController.CreateReviewComment);
 
