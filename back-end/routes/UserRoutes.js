d3651c0c508cb2c1395ed49c7b2dbd4344dce315
OyungerelA <oa853@nyu.edu>
2022-11-09 01:46:01 -0500

    add routes for MyRecipe

--- back-end/routes/UserRoutes.js
+++ back-end/routes/UserRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
+const MyRecipeController = require('../controllers/MyRecipeController');
 const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/test', UserController.TestControllerFunction);
+router.get('/test', UserController.TestControllerFunction, MyRecipeController.TestMyRecipeFunction);
 router.post('/register', UserController.RegisterUser);
 router.post('/login', UserController.LoginUser);
-router.get('/profile', authenticate, UserController.GetProfile);
+router.get('/profile', authenticate, UserController.GetProfile, MyRecipeController.GetMyRecipe);
 
 module.exports = router;
