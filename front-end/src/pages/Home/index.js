9778fd9ac62cd2118b781e63a80a86e15def63ee
yeonieheoo <sh5874@nyu.edu>
2022-11-09 01:53:21 -0500

    Home Page Integration

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -3,16 +3,16 @@ import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 // fetched all data from the API below into a file: '../../temp_recipedata.json'
 import Recipe from '../../components/Recipe';
-import StringConfig from '../../StringConfig';
+// import StringConfig from '../../StringConfig';
 import '../../components/Recipe/aRecipeButtonStyle.css';
 import './Home.css';
-import * as Util from '../../util';
+import { fetchRecipeData } from '../../util';
 
 function Home(): React.Node {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    Util.fetchRecipeData(setData);
+    fetchRecipeData(setData);
   }, []);
 
   const navigate = useNavigate();
@@ -22,9 +22,9 @@ function Home(): React.Node {
       {/* <h1>Recommended Recipes</h1> */}
       <section className="recipes container-fluid row justify-content-center">
         <h1 className="mb-5"><strong>Select Your Recipes</strong></h1>
-        {data.length > 0 ? data.map((item, ind) => (
-          <Recipe key={ind} details={item} onAction={() => navigate(`recipe/${ind}`)} />
-        )) : StringConfig.API_FAILURE_WARNING}
+        {data.map((item) => (
+          <Recipe key={item.index} details={item} onAction={() => navigate(`recipe/${item.index}`)} />
+        ))}
       </section>
     </>
   );
