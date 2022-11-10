3c359924b134b26307c3210b17a1147ab46afa5a
NghiaNim <ntn6039@nyu.edu>
2022-11-10 11:21:53 -0500

    uncomment working code

--- back-end/routes/likeRoutes.js
+++ back-end/routes/likeRoutes.js
@@ -4,6 +4,6 @@ const LikeController = require('../controllers/LikeController');
 const router = express.Router();
 
 router.get('/test', LikeController.TestLikeFunction);
-// router.post('/like', LikeController.);
+router.post('/like', LikeController.CreateLike);
 
 module.exports = router;
