4d9d4fcaa51e0366068f5ec12164c010554e27e3
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-14 01:06:55 -0500

    Revamped navbar UI. Next is Login/Register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,11 +5,33 @@ import Navbar from './components/Navbar';
 import './App.css';
 
 function App(): React.Node {
+  const AppRoutes = [
+    {
+      routePath: '/',
+      title: 'Home',
+      icon: 'home',
+    },
+    {
+      routePath: '/search-ingredient',
+      title: 'Search',
+      icon: 'search',
+    },
+    {
+      routePath: '/profile',
+      title: 'Profile',
+      icon: 'person',
+    },
+    {
+      routePath: '/',
+      title: 'Recipe',
+      icon: 'feed',
+    },
+  ];
+
   return (
-    <>
-      <Navbar />
-      <footer />
-    </>
+    <Navbar
+      AppRoutes={AppRoutes}
+    />
   );
 }
 
