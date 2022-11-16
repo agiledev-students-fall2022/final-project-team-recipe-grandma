850fcb02d6e2739d24fea636dc85c7bdba19f6c9
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-15 21:43:33 -0500

    Finished authentication flow

--- front-end/src/components/ProtectedRoutes/index.js
+++ front-end/src/components/ProtectedRoutes/index.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectCurrentToken } from '../../features/auth/authSlice';
 
 type Props = {|
   RequireAuthOrLogout: boolean
@@ -11,10 +13,10 @@ const defaultProps = {
 
 function ProtectedRoutes(props: Props): React.Node {
   const { requireAuthOrLogout } = props;
-  const auth = { token: false }; // Fix later
+  const token = useSelector(selectCurrentToken);
 
-  const requireAuth = auth.token ? <Outlet /> : <Navigate to="/login" />;
-  const requireLogout = !auth.token ? <Outlet /> : <Navigate to="/" />;
+  const requireAuth = token ? <Outlet /> : <Navigate to="/login" />;
+  const requireLogout = !token ? <Outlet /> : <Navigate to="/" />;
   return requireAuthOrLogout ? requireAuth : requireLogout;
 }
 
