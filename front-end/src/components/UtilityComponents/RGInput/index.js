47d9bc4db1734cbdfa8239e5e7739191125e2fd5
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-16 19:18:12 -0500

    Updated recipes page and UserController bugs

--- front-end/src/components/UtilityComponents/RGInput/index.js
+++ front-end/src/components/UtilityComponents/RGInput/index.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './RGInput.css';
 
 type Props = $ReadOnly<{|
+  error?: boolean,
   label?: string,
   onChange?: () => void,
   required?: boolean,
@@ -13,18 +14,21 @@ const defaultProps = {
   label: '',
   onChange: () => null,
   required: false,
+  error: false,
   type: 'text',
 };
 
 function RGInput(props: Props): React.Node {
   const {
+    error,
     label,
     onChange,
     required,
     type,
     value,
   } = props;
-  const className = (value !== '') ? 'rg-input-box rgi-box rgi-labeled' : 'rg-input-box rgi-box';
+  const baseClassName = (value !== '') ? 'rg-input-box rgi-box rgi-labeled' : 'rg-input-box rgi-box';
+  const className = error ? `${baseClassName} rgi-error` : baseClassName;
   return (
     <div className={className}>
       <input
