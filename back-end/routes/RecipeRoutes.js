cf8064e0b21f7f16fe52a449643cf430c3317aa8
yeonieheoo <sh5874@nyu.edu>
2022-11-18 15:35:59 -0500

    DB - Recipes ToDo list

--- back-end/routes/RecipeRoutes.js
+++ back-end/routes/RecipeRoutes.js
@@ -3,9 +3,16 @@ const RecipeController = require('../controllers/RecipeController');
 
 const router = express.Router();
 
+// TO-DO list before next tuesday
+// 1) show user's created recipes on front-end
+// Created recipes are now successfully uploaded to MongoDB
+// 2) recipe recommending based on user's likes
+// Filter recommended recipes
+// Return the exact recommended recipes only
+
 // test
 router.get('/test', RecipeController.TestRecipeFunction);
-// create new recipe
+// create new recipe - HALF DONE (11/18)
 router.post('/create', RecipeController.NewRecipe);
 // get all recipes - DONE (11/18)
 router.get('/all', RecipeController.GetRecipes);
