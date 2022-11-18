e9918167df8cd57c392377c928f7fbf1d503a1dc
yeonieheoo <sh5874@nyu.edu>
2022-11-18 15:52:40 -0500

    DB - Updated Recipes ToDo list

--- back-end/routes/RecipeRoutes.js
+++ back-end/routes/RecipeRoutes.js
@@ -3,22 +3,26 @@ const RecipeController = require('../controllers/RecipeController');
 
 const router = express.Router();
 
-// TO-DO list before next tuesday
-// 1) show user's created recipes on front-end
-// Created recipes are now successfully uploaded to MongoDB
-// 2) recipe recommending based on user's likes
-// Filter recommended recipes
-// Return the exact recommended recipes only
+// TO-DO list
+// ========================================
+// YEONIE - DONE: Allow fetch by ID
+// YEONIE - DONE: Allow fetch All or fetch via pagination
+// Allow fetch recipes by user ID (this can should fall under ownership of user routes)
+// Allow fetch recommended recipes algorithmically
+// Link recipe creation to front-end
+// YEONIE - DONE: Add recipe deletion
+// Add recipe updat/edit by single property or more in one dynamic route
+// Link all functionalities to front-end
 
 // test
 router.get('/test', RecipeController.TestRecipeFunction);
-// create new recipe - HALF DONE (11/18)
+// create new recipe
 router.post('/create', RecipeController.NewRecipe);
-// get all recipes - DONE (11/18)
+// delete a recipe
+router.get('/delete/:index', RecipeController.DeleteRecipe);
+// get all recipes
 router.get('/all', RecipeController.GetRecipes);
-// single recipe - DONE (11/18)
+// single recipe
 router.get('/:index', RecipeController.SingleRecipe);
 // recipe recommended based on user's likes
 router.get('/recommendation', RecipeController.RecommendedRecipe);
-
-module.exports = router;
