d5ac86fbb8637533d5e74c7116c161bb049f5c7a
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-11-18 07:50:05 -0500

    Name your files better nerds

--- back-end/routes/LikeRoutes.js
+++ back-end/routes/LikeRoutes.js
@@ -3,7 +3,6 @@ const LikeController = require('../controllers/LikeController');
 
 const router = express.Router();
 
-router.get('/test', LikeController.TestLikeFunction);
 router.post('/like', LikeController.CreateLike);
 
 module.exports = router;
