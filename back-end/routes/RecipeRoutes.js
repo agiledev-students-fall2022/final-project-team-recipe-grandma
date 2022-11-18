445fc6ac5b2b8633b9647346d679ee790adca57e
yeonieheoo <sh5874@nyu.edu>
2022-11-18 16:32:22 -0500

    DB - Added UpdateRecipe function

--- back-end/routes/RecipeRoutes.js
+++ back-end/routes/RecipeRoutes.js
@@ -9,10 +9,13 @@ const router = express.Router();
 // YEONIE - DONE: Allow fetch All or fetch via pagination
 // Allow fetch recipes by user ID (this can should fall under ownership of user routes)
 // Allow fetch recommended recipes algorithmically
-// Link recipe creation to front-end
 // YEONIE - DONE: Add recipe deletion
-// Add recipe updat/edit by single property or more in one dynamic route
+// YEONIE - WORKING: Add recipe update/edit by single property or more in one dynamic route
+
+// FRONT-END work for later
+// ========================================
 // Link all functionalities to front-end
+// Link recipe creation to front-end
 
 // test
 router.get('/test', RecipeController.TestRecipeFunction);
@@ -20,6 +23,8 @@ router.get('/test', RecipeController.TestRecipeFunction);
 router.post('/create', RecipeController.NewRecipe);
 // delete a recipe
 router.get('/delete/:index', RecipeController.DeleteRecipe);
+// update a recipe
+router.get('/update/:index', RecipeController.UpdateRecipe);
 // get all recipes
 router.get('/all', RecipeController.GetRecipes);
 // single recipe
