1583532fff475d8aa3cb1573d12edbafd8ca4d35
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-21 23:45:47 -0500

    Added auth to routes that currently "work"

--- back-end/routes/LikeRoutes.js
+++ back-end/routes/LikeRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const LikeController = require('../controllers/LikeController');
 
 const router = express.Router();
+const { authenticate } = require('../middleware/auth');
 
-router.post('/like', LikeController.CreateLike);
+
+router.post('/like', authenticate, LikeController.CreateLike);
 
 module.exports = router;
