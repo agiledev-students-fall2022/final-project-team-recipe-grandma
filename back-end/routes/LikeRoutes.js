6c22ff36c6fa0297ded36afd827a109a8f617dbc
Mohammed Ajao <mohammedajao@Mohammeds-MBP.fios-router.home>
2022-11-22 00:01:11 -0500

    Added auth middleware to more stuff and deleted overhead

--- back-end/routes/LikeRoutes.js
+++ back-end/routes/LikeRoutes.js
@@ -4,7 +4,6 @@ const LikeController = require('../controllers/LikeController');
 const router = express.Router();
 const { authenticate } = require('../middleware/auth');
 
-
 router.post('/like', authenticate, LikeController.CreateLike);
 
 module.exports = router;
