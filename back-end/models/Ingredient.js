a4f99d5dad318556485a1d39e6f6387b98736647
Mohammed Ajao <mohammedadrianajao@gmail.com>
2022-11-28 07:26:33 -0500

    Added dynamic ingredient creation to recipe creation

--- back-end/models/Ingredient.js
+++ back-end/models/Ingredient.js
@@ -4,15 +4,12 @@ const Ingredient = mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a ingredient'],
+    unique: true,
   },
-  ingredientType: {
+  type: {
     type: String,
-    required: true,
+    required: [true, 'Please add a ingredient type'],
   },
-  // type: {
-  //   type: String,
-  //   require: [true, 'Please add a ingredient type'],
-  // },
 });
 
 module.exports = mongoose.model('Ingredient', Ingredient);
