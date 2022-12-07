6ada94976f26d21852582d1e8d656368d8d96770
geontackee <95744496+geontackee@users.noreply.github.com>
2022-12-07 12:05:06 -0500

    Added to preven CORS error.

--- back-end/routes/UserRoutes.js
+++ back-end/routes/UserRoutes.js
@@ -5,6 +5,15 @@ const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
 router.get('/test', UserController.TestControllerFunction);
 router.post('/register', UserController.RegisterUser);
 router.post('/login', UserController.LoginUser);
