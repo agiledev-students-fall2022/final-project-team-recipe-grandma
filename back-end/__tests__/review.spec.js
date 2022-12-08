01f9de2dbe03d1566439c68f9fb7b21bbae0d79f
geontackee <95744496+geontackee@users.noreply.github.com>
2022-12-08 00:21:14 -0500

    Modified review tests.

--- back-end/__tests__/review.spec.js
+++ back-end/__tests__/review.spec.js
@@ -7,6 +7,7 @@ const {
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const server = require('../index');
+const { ObjectId } = require('mongodb');
 
 chai.use(chaiHttp);
 
@@ -18,77 +19,63 @@ after((done) => {
 
 // make sure to replace the testUserCredentials with what you have in your database
 const testUserCredentials = {
-  email: 'gtl256@nyu.edu',
-  password: 'iawVv9CxWhJcwN7',
-  name: 'geontack',
+  email: 'howdy@nyu.edu',
+  password: 'howdy',
+  name: 'howdy',
 };
 
-let testUser;
-before((done) => {
-  chai.request(server)
-    .post('/rgapi/user/login')
-    .type('form')
-    .send({
-      email: testUserCredentials.email,
-      password: testUserCredentials.password,
-      name: testUserCredentials.name,
-    })
-    .end((err, res) => {
-      expect(res).to.have.status(200);
-      testUser = res.body;
+after ((done) => {
+  mongoose.connection.close();
+  server.close();
+  done();
+});
+
+
+let token;
+describe('Log in', () => {
+  it('should register a user', (done) => {
+    chai.request(server)
+      .post('/rgapi/user/register')
+      .type('form')
+      .send({
+        name: testUserCredentials.name,
+        email: testUserCredentials.email,
+        password: testUserCredentials.password,
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+      });
+    done();
+  })
+  it('should log in', (done) => {
+    chai.request(server)
+      .post('/rgapi/user/login')
+      .type('form')
+      .send({
+        email: testUserCredentials.email,
+        password: testUserCredentials.password,
+        name: testUserCredentials.name,
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        token = res;
+        console.log(res);
+      });
       done();
-    });
-});
-
-describe('GET /rgapi/review/database/:index', () => {
-  it('Should return the reviews of the desired recipe by index', (done) => {
-    const currIndex = 0;
-    chai.request(server)
-      .get(`/rgapi/review/database/${currIndex}`)
-      .set({ Authorization: `Bearer ${testUser.token}` })
-      .end((err, res) => {
-        res.should.have.status(200);
-        const {
-          id, body, stars, username, parentId,
-        } = res.body.reviews[0];
-        expect(id).to.not.be.a('null');
-        expect(body).to.be.a('string');
-        expect(stars).to.be.a('number');
-        expect(username).to.be.a('string');
-        expect(parentId).to.be.a('string');
-        done();
-      });
   });
-});
-
-describe('GET /rgapi/review/database/:invalidIndex', () => {
-  it('Should return status 400 since index is invalid', (done) => {
-    const falseIndex = -1;
-    chai.request(server)
-      .get(`/rgapi/review/database/${falseIndex}`)
-      .set({ Authorization: `Bearer ${testUser.token}` })
-      .end((err, res) => {
-        res.should.have.status(400);
-        done();
-      });
-  });
-});
-
-describe('POST /rgapi/review/review/create', () => {
-  it('Should create a new review an upload it to database', (done) => {
+  it('Should create a new review an upload it to database', () => {
     chai.request(server)
       .post('/rgapi/review/review/create')
-      .set({ Authorization: `Bearer ${testUser.token}` })
+      .set('Authorization', `Bearer ${token}`)
       .type('form')
       .send({
         body: 'Test',
         stars: 1,
         username: 'Test',
-        parentId: '0',
+        parentId: new mongoose.Schema.Types.ObjectId('test'),
       })
       .end((err, res) => {
-        expect(res).to.have.status(201);
-        done();
+        expect(res).to.have.status(403);
       });
   });
 });
